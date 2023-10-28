044d70bda33a926e7262e2b91b74f394dfd90557
Cathy Mosinski <jcmosinski@gmail.com>
2023-10-27 21:07:49 -0400

    updating github#

---
+++ recipeListResults.js
@@ -0,0 +1,343 @@
+export const data = {
+  "results": [
+    {
+      "title": "Classic Hot Dogs",
+      "image": "https://images.squarespace-cdn.com/content/v1/552f35bee4b0955308211538/1431945925984-E3BC4EQPQM1XY40NCC9K/image-asset.png",
+      "ingredients": {
+        "hotdog": "1",
+        "hotdog bun": "1",
+        "ketchup": "1 oz"
+      },
+      "totalTimeMin": "5",
+      "cookTimeMin": "4",
+      "prep-timeMin": "1",
+      "instructions": "Heat up the hot dog in the microwave, stove top or on the grill. Wrap the bun in a papertowel and pop it in the microwave for 30 seconds. Put the two together and top with ketchup for the perfect easy meal!",
+      "keywords": ["fast", "easy"],
+      "website": "https://potatorolls.com/recipes/classic-hot-dog/"
+    },
+    {
+      "title": "Ceasar Salad",
+      "image": "https://spoonacular.com/recipeImages/636682-312x231.jpg",
+      "ingredients": {
+        "lettuce": "1 head",
+        "ceasar dressing": "2 oz",
+        "croutons": "6 pieces"
+      },
+      "totalTimeMin": "2",
+      "cookTimeMin": "0",
+      "prep-timeMin": "2",
+      "instructions": "Cut lettuce into bite sized pieces. Toss with dressing in a bowl. Sprinkle croutons on top and serve.",
+      "keywords": ["fast", "easy", "vegetarian"],
+      "website": "https://www.bonappetit.com/recipe/classic-caesar-salad"
+    },
+    {
+      "title": "Digornos Pizza",
+      "image": "https://d332juqdd9b8hn.cloudfront.net/wp-content/uploads/2019/09/digiorno-e1569861704936.jpg",
+      "ingredients": {
+        "pizza": "1"
+      },
+      "totalTimeMin": "21",
+      "cookTimeMin": "21",
+      "prep-timeMin": "0",
+      "instructions": "Pre-heat oven to 400 degrees. Remove pizza from plastic wrapper. Bake pizza for 18 to 21 minutes. Enjoy!",
+      "keywords": ["fast", "easy"],
+      "website": "https://www.goodnes.com/digiorno/products/rising-crust-pepperoni-pizza/"
+    },
+    {
+      "title": 'Chicken Pasta Bake',
+      "image": "https://gimmedelicious.com/wp-content/uploads/2020/04/4-Ingredient-Chicken-Pasta-2329.jpg",
+      "ingredients": {
+        'chicken': "2 cups",
+        'pasta': "4 cups",
+        'red sauce': "16 oz",
+        'parmesean': "4 oz",
+        'mozzarella': "1.5 cups",
+        'mushrooms': "8 oz",
+        'onions': "1 medium",
+        'garlic': "4 cloves"
+      },
+      "totalTimeMin": "55",
+      "cookTimeMin": "20",
+      "prepTimeMin": "35",
+      "instructions": 'Precook pasta and chicken. Mix in baking dish. Cook for 15-20 min at 350 degrees until cheese melts.',
+      "keywords": ["chicken", "comfort food"],
+      "website": 'https://gimmedelicious.com/4-ingredient-chicken-pasta-casserole/'
+    },
+    {
+      "title": 'Chicken Enchilada Bake',
+      "image": "https://i0.wp.com/bobbiskozykitchen.com/wp-content/uploads/2022/08/slow2Bcooker2Bchicken2Benchilada2Bquinoa2B2.jpg",
+      "ingredients": {
+        'chicken': "1 lb",
+        'black beans': "15.5 oz",
+        'enchilada sauce': "2 cups",
+        'green chilis': "4 oz",
+        'onion': "1 medium",
+        'garlic': "2 cloves",
+        'cheddar': "3/4 cup",
+        'pepper jack': "3/4 cup",
+        'quinoa': "1.5",
+        'corn': "1 cup (frozen)"
+      },
+      "totalTimeMin": "60",
+      "cookTimeMin": "25",
+      "prepTimeMin": "35",
+      "instructions": 'Precook the chicken and the quinoa. Saute onions and chilis. Mix in bakeware with beans and cheese. Bake for 25 minutes at 375.',
+      "keywords": ['chicken', "mexican", "easy"],
+      "website": 'https://www.bobbiskozykitchen.com/2015/01/slow-cooker-chicken-enchilada-quinoa.html'
+    },
+    {
+      "title": 'Chicken Teriaki with Broccoli',
+      "image": "https://www.wellplated.com/wp-content/uploads/2016/02/Crock-Pot-Teriyaki-Chicken.jpg",
+      "ingredients": {
+        'chicken': "1.5 lbs",
+        'soy sauce': "2/3 cup",
+        'rice vinegar': "3 TBSP",
+        'honey': "3 TBSP",
+        'brown sugar': "3 TBSP",
+        'garlic': "1 clove",
+        'ginger': "1 TBSP",
+        'broccoli': "2 crowns",
+        'rice': "1 cup",
+        'green onions': "2"
+      },
+      "totalTimeMin": "250",
+      "cookTimeMin": "240",
+      "prepTimeMin": "10",
+      "instructions": 'Cook chicken on low for four hours in teriyaki sauce. Remove chicken and shred. Serve over bed of rice/quinoa and broccoli with green onions to taste.',
+      "keywords": ['chicken'],
+      "website": 'https://www.wellplated.com/crock-pot-teriyaki-chicken/'
+    },
+    {
+      "title": 'Garlic Honey Chicken',
+      "image": "https://www.wellplated.com/wp-content/uploads/2019/03/Healthy-Slow-Cooker-Honey-Garlic-Chicken.jpg",
+      "ingredients": {
+        'chicken': "1.5 lbs",
+        'honey': "1/3 cup",
+        'garlic': "4 cloves",
+        'soy sauce': "1/3 cup",
+        'tomato paste': "2 TBSP",
+        'chili flakes': "2 tsp",
+        'rice': "1 cup",
+        'green onions': "2",
+        'sesame seeds': "1 TBSP",
+        'rice vinegar': "1 TBSP",
+        'cornstarch': "2 TBSP",
+        'broccoli': "2 crowns"
+      },
+      "totalTimeMin": "370",
+      "cookTimeMin": "360",
+      "prepTimeMin": "10",
+      "instructions": 'Whisk "ingredients" for sauce together and pour over raw chicken in slow cooker. Cook on low 6 hours. Remove chicken and whisk in cornstarch to thicken sauce. Serve over rice and broccoli',
+      "keywords": ['chicken', "slow cooker"],
+      "website": 'https://www.wellplated.com/slow-cooker-honey-garlic-chicken/'
+    },
+    {
+      "title": 'Baked Chicken wtih Spinach Ravioli, spinach and mushrooms',
+      "image": "https://n6a7k3g3.rocketcdn.me/wp-content/uploads/2019/03/Ravioli-with-Chicken-and-Mushrooms-56.jpg",
+      "ingredients": {
+        'chicken': "1 lb",
+        'spinach': "2 cups",
+        'mushrooms': "1 cup",
+        'spinach and cheese ravioli': "1 pkg",
+        'parmesean cheese': "1 oz",
+        'onion': "1 medium",
+        'garlic': "4 cloves"
+      },
+      "totalTimeMin": "25",
+      "cookTimeMin": "15",
+      "prepTimeMin": "10",
+      "instructions": 'Precook chicken. Boil raviolis for 8 minutes. While boiling, saute onions and garlic, then add mushrooms, chicken and parmesean cheese. Once thickened, add spinach and stir gently.',
+      "keywords": ['chicken'],
+      "website": 'https://www.bellylaughliving.com/ravioli-with-chicken-and-spinach/'
+    },
+    {
+      "title": 'Chicken Taco Salads',
+      "image": "https://www.closetcooking.com/wp-content/uploads/2014/04/Slow-Cooker-Chicken-Carnitas-800-5955.jpg",
+      "ingredients": {
+        'chicken': "3 lbs",
+        'salsa': "16 oz",
+        'lime': "2",
+        'taco seasoning': "1 pkt",
+        'cilantro': "3 TBSP",
+        'black beans': "14 oz",
+        'cumin': "2 tsp",
+        'sliced onion': "1 med/large",
+        'sliced peppers': "2"
+      },
+      "totalTimeMin": "430",
+      "cookTimeMin": "420",
+      "prepTimeMin": "10",
+      "instructions": 'In slow cooker, mix salsa, lime juice, taco seasoning and cilantro. Salt and pepper the chicken (add some cumin), then mix into sauce. Chop slices of onion and peppers to add to pot with black beans as well. Cook on low 7-8 hours. Shred chicken with beaters, remove bones and serve.',
+      "keywords": ['chicken', "mexican"],
+      "website": 'https://tasty.co/recipe/slow-cooker-shredded-tacos'
+    },
+    {
+      "title": 'Garlic Parmesean Chicken',
+      "image": "https://hips.hearstapps.com/del.h-cdn.co/assets/16/07/1455837174-delish-slow-cooker-chicken-1.jpg?crop=1xw:0.99975xh;center,top&resize=980:*",
+      "ingredients": {
+        'chicken': "2 lb",
+        'red potatoes': "1 lb",
+        'butter': "2 TBSP",
+        'olive oil': "3 TBSP",
+        'garlic': "5 cloves",
+        'onion powder': "2 TBSP",
+        'thyme': "2 TBSP",
+        'parsley': "2 tsp",
+        'rosemary': "2 tsp",
+        'parmesean': "2 TBSP"
+      },
+      "totalTimeMin": "495",
+      "cookTimeMin": "480",
+      "prepTimeMin": "15",
+      "instructions": 'In large skillet over med-high heat cook chicken in 1 TBSP of olive oil, season with your choice of seasoning, sear 3 min per side. Meanwhile, toss potatoes with 2TBSP oil, butter, EXTRA garlic and onion powder, thyme, parsley/rosemary, and parmesean. Season generously with salt and pepper, Add chicken and cook on low for 8 hours. Garnish with parmesean before serving.',
+      "keywords": ['chicken'],
+      "website": 'https://www.delish.com/cooking/recipe-ideas/recipes/a46066/slow-cooker-garlic-parmesan-chicken-recipe/'
+    },
+    {
+      "title": 'Mild or Buffalo Chicken',
+      "image": "https://foodfolksandfun.net/wp-content/uploads/2021/02/Oven-Baked-Chicken-Thighs.jpg",
+      "ingredients": {
+        'chicken': "2 lbs",
+        'mild sauce': "4 TBSP",
+        'buffalo dip': "8 TBSP"
+      },
+      "totalTimeMin": "40",
+      "cookTimeMin": "35",
+      "prepTimeMin": "5",
+      "instructions": 'Cook chicken at 400 degrees for 30 minutes. For the last 5 minutes, place a dollop of buffalo dip on each piece.',
+      "keywords": ['chicken'],
+      "website": 'https://foodfolksandfun.net/oven-baked-chicken-thighs/'
+    },
+    {
+      "title": 'Spaghetti Squash with meatballs',
+      "image": "https://alldayidreamaboutfood.com/wp-content/uploads/2015/05/Slow-Cooker-Spaghetti-Squash-and-Meatballs-4.jpg",
+      "ingredients": {
+        'meatballs': "1/2 bag",
+        'olive oil': "2 TBSP",
+        'speghetti squash': "1 medium",
+        'garlic': "1/2 tsp",
+        'oregano': "1/4 tsp",
+        'crushed tomatoes': "1.5 cups"
+      },
+      "totalTimeMin": "430",
+      "cookTimeMin": "420",
+      "prepTimeMin": "10",
+      "instructions": 'Cut spaghetti squash in half, shortwise. Place in bottom of slow cooker, cut side down. Make marainara sauce in blender (combine tomatoes, salt, garlic powder, pepper and oregano), blend until smooth. Pour into bottom of slow cooker. Place "meatballs over tomatoes and around squash. Cook on low for 7 hours. Use tongs and gloves to remove squash, scoop out seeds and discard. Scoop out flesh into colander to let moisture drain. Split between 4 plates, top with sauce and meatballs.',
+      "keywords": ['meatballs'],
+      "website": 'https://alldayidreamaboutfood.com/low-carb-slow-cooker-spaghetti-squash-and-meatballs/'
+    },
+    {
+      "title": 'Tortalini Vegetable Soup',
+      "image": "https://i0.wp.com/myvegetarianroots.com/wp-content/uploads/2019/12/DSC_0017-1.jpeg",
+      "ingredients": {
+        'olive oil': "1/4 cup",
+        'onion': "1",
+        'garlic': "4 cloves",
+        'thyme': "2 tsp",
+        'basil': "1 TBSP",
+        'oregano': "1 TBSP",
+        'paprika': "1 tsp",
+        'red pepper flakes': "1 pinch",
+        'tomato paste': "2 TBSP",
+        'carrots': "6",
+        'celery': "4 stalks",
+        'dry white wine': "1/2 cup",
+        'low sodium vegetable broth': "6 cups",
+        'kale': "4 cups",
+        'whole milk/heavy cream': "1/2 cup",
+        'parmesean cheese': "1/3 cup",
+        'tortelliini': "1 lb"
+      },
+      "totalTimeMin": "500",
+      "cookTimeMin": "480",
+      "prepTimeMin": "20",
+      "instructions": 'heat olive oil in large pot over medium heat and cook onion, garlic, thyme, basil, oregano, paprika, and pinch of salt and pepper. Cook 5 min until very fragrant. Remove from heat and add to crockpot. Stir in tomato paste, carrots, and celery. Add wine, broth, and pinch of red pepper flakes. Cover and cook on low for 8 hours. During last 15 minutes of cooking, stir in kale, cream, parmesean and tortellini. Serve topped with parmesean and fresh oregano.',
+      "keywords": ['vegetarian', "soup"],
+      "website": 'https://www.brit.co/slow-cooker-tortellini-vegetable-soup/'
+    },
+    {
+      "title": 'Eggplant Lasagna',
+      "image": "https://www.wellplated.com/wp-content/uploads/2016/07/Crock-Pot-Low-Carb-Lasagna.jpg",
+      "ingredients": {
+        'eggplant': "1 medium",
+        'zucchini': "2 medium",
+        'red sauce': "16 oz",
+        'red onion': "1 medium",
+        'red pepper': "1",
+        'lowfat cottage cheese': "16 oz",
+        'eggs': "2 large",
+        'mozzarella': "8 oz",
+        'parmesean': "1 oz"
+      },
+      "totalTimeMin": "180",
+      "cookTimeMin": "150",
+      "prepTimeMin": "30",
+      "instructions": 'Cut eggplant and zucchini lengthwise into 1/4 inch slices that resemble lasagna noodles. Spread slices onto kitchen towels and sprinkle lightly with salt. Let stand for 15 minutes and pat dry. Grill slices 2-3 minutes per side. OPTIONAL: brown some ground turkey or chicken to make the redsauce into a emat sauce. Lightly coat crockpot with non-stick spray. Spread 1/2 cup tomato sauce on bottom then 1 layer eggplant noodles. Beat cottage cheese and eggs together in small bowl. put 1/3 of mixture over eggplants. 1/3 peppers and onions, 1/3 mozzarella. Next do the same steps but replace eggplant with zucchini. Tomatoe sauce, zucchini, cheese mixture, peppers and onions, and lastly mozzarella. Lastly do tomatoe sauce, 1 layer of eggplant “noodles”; remaining cottage cheese; remaining bell peppers and onions; 1 layer of zucchini noodles; 1/2 cup tomato sauce; remaining mozzarella. Cook on high 3 hours until eggplant is tender',
+      "keywords": ['vegetarian', "low carb"],
+      "website": 'https://www.wellplated.com/low-carb-lasagna/'
+    },
+    {
+      "title": 'White Chicken Chili',
+      "image": "https://www.cookingclassy.com/wp-content/uploads/2018/12/white-chicken-chili-1.jpg",
+      "ingredients": {
+        'chicken': "2 lbs",
+        'onion': "1 large",
+        'celery': "2 stalks",
+        'garlic': "4 cloves",
+        'green bell peppers': "3/4 cup",
+        'jalapeno': "3 TBSP",
+        'cumin': "1 TBSP",
+        'coriander': "1/2 tsp",
+        'oregano': "1/2 tsp",
+        'salt': "2 tsp",
+        'low sodium chicken broth': "29 oz",
+        'great northern beans': "30 oz",
+        'frozen corn': "1 cup"
+      },
+      "totalTimeMin": "315",
+      "cookTimeMin": "300",
+      "prepTimeMin": "15",
+      "instructions": 'Place raw chicken, diced onion, chopped celery, chopped bell pepper, garlic, diced jalapeno, spices and chicken broth into crock pot to cook for 4 hours. Remove chicken and shred or chop the meat, return to slow cooker with beans and frozen corn to cook for an additional 1 hour. Serve and enjoy!',
+      "keywords": ['chicken', "slow cooker"],
+      "website": 'https://www.simplywhisked.com/slow-cooker-white-chicken-chili/'
+    },
+    {
+      "title": 'Three Cheese Spaghetti Squash',
+      "image": "https://alldayidreamaboutfood.com/wp-content/uploads/2016/11/Slow-Cooker-Three-Cheese-Spaghetti-Squash.jpg",
+      "ingredients": {
+        'spaghetti squash': "1 large",
+        'butter': "1/4 cup",
+        'garlic': "2 cloves",
+        'asiago cheese': "1.5 oz",
+        'parmesean cheese': "1.5 oz",
+        'mozzarella': "3/4 cup",
+        'basil': "1/4 cup",
+        'salt': "1 tsp",
+        'pepper': "1/2 tsp"
+      },
+      "totalTimeMin": "380",
+      "cookTimeMin": "360",
+      "prep-timeMin": "20",
+      "instructions": 'This is a side dish. Cut squash shortwise and place cut side down into slow cooker. Cook for 6 hours on low, squash is fully cooked when it can be easily squeezed. Remove squash and add butter and garlic to slow cooker. Meanwhile, scoop out seeds and discard, then scoop out flesh and add back into slow cooker. Add asiago, parmesean, salt and pepper. toss to combine "ingredients" and spread evenly over bottom of slow cooker. Sprinkle with mozzarella and replace the lid, let sit in warm slow cooker until melted (~10 min). Serve with fresh basil.',
+      "keywords": ['vegetarian', "slow cooker"],
+      "website": 'https://alldayidreamaboutfood.com/low-carb-slow-cooker-spaghetti-squash/'
+    }
+  ]
+}
+
+const SchemaRecipe =
+{
+  "title": "",
+  "image": "",
+  "ingredients": {
+    "": "",
+    "": "",
+    "": ""
+  },
+  "totalTimeMin": "",
+  "cookTimeMin": "",
+  "prep-timeMin": "",
+  "instructions": "",
+  "keywords": ["", ""],
+  "website": ""
+}
