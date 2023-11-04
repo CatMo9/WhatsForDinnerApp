9819b51899b14b262fc2cd8337e5e6cc7cc7dbf6
Cathy Mosinski <jcmosinski@gmail.com>
2023-11-04 14:05:58 -0400

    controllers started for working with db

--- server/db/index.js
+++ server/db/index.js
@@ -10,22 +10,3 @@ mongoose
   .catch((e) => {
     console.log(e.message);
   });
-
-// saveNewRecipe(recipeInfo) => {
-//   // const recipeObject = {
-//   //   title: recipeInfo.title,
-//   //   image: recipeInfo.image,
-//   //   ingredients: recipeInfo.ingredients,
-//   //   totalTimeMin: recipeInfo.totalTimeMin,
-//   //   cookTimeMin: recipeInfo.cookTimeMin,
-//   //   prepTimeMin: recipeInfo.prepTimeMin,
-//   //   instructions:recipeInfo.instructions,
-//   //   keywords:recipeInfo.keywords,
-//   //   website:recipeInfo.website
-//   // }
-
-//   //does this work to save a newRecipe with the recipeInfo from the function? Or do I need to create a new Object? (If so use recipeObject from above)
-//   const newRecipe = new Recipe(recipeInfo)
-
-//   newRecipe.save().then(()=>(console.log("Recipe has been saved"))).catch((e)=> console.log(e.message))
-// }
